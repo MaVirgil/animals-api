414994b78df035844390e933d0713df7e94635f4
MaVirgil <magnusvirgil@hotmail.com>
2026-09-12 11:27:36 +0200

    add json parsing middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express =  require('express')
+const animalList = require('./data/animals')
 const animalsRouter = require('./route/animals.route')
 const PORT_NUMBER = 8080;
 
 const app = express();
+app.use(express.json);
 
 app.get('/', (req, res) => {
   res.send({
-    data: "ok"
+    data: "connected"
   });
 })
 
